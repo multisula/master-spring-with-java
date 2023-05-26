060bcf9b58889e7c5bce59ebfea9f313683b0255
multisula <multisula@gmail.com>
2023-05-26 12:23:18 +0900

    understanding loose coupling and tight coupling

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
@@ -1,9 +1,9 @@
 package com.in28minutes.learnspringframework.game;
 
 public class GameRunner {
-    MarioGame game;
+    SuperContraGame game;
 
-    public GameRunner(MarioGame game) {
+    public GameRunner(SuperContraGame game) {
         this.game = game;
     }
 
