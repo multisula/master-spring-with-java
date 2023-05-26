8b5ec7bceceee833c715307f90c83a4032a166cc
multisula <multisula@gmail.com>
2023-05-26 14:33:47 +0900

    multipleBeans, Primary, Qualifier

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -5,6 +5,8 @@ import com.in28minutes.learnspringframework.game.PacmanGame;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class App02HelloWorldSpring {
     public static void main(String[] args) {
         // 1: Launch a Spring Context -
@@ -16,9 +18,14 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
         System.out.println(context.getBean("address2"));
-//        System.out.println(context.getBean(Address.class));
         System.out.println(context.getBean("person2MethodCall"));
         System.out.println(context.getBean("person3Parameters"));
+        System.out.println(context.getBean("person5Qualifier"));
 
+        System.out.println(context.getBean(Person.class));
+        System.out.println(context.getBean(Address.class));
+
+//        Arrays.stream(context.getBeanDefinitionNames())
+//                .forEach(System.out::println);
     }
 }
